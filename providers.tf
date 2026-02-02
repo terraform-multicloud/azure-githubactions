73c7114037ff18b01aeddd4ce4fc90e46c508862
himanshu <rathihimanshu@live.com>
2026-02-02 17:41:58 +0530

    backend

---
+++ providers.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "himanshu-resource-group-one"
+    storage_account_name = "myterraformbackendsa"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
+}
+
+
+provider "azurerm" {
+    features {}
+  
+}
+resource "azurerm_resource_group" "one" {
+    name     = "himanshu-resource-githubaction"
+    location = "East US"
+}
